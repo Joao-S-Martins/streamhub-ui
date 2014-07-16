e159737dfde51ea2dba8230a600e802df3ad6829
Ryan Cheung <ryanc@livefyre.com>
2014-07-16 12:57:43 -0700

    Move ShareMenu to streamhub-share module. Move streamhub-ui/menu/base to streamhub-ui/menu

--- src/main.js
+++ src/main.js
@@ -1,6 +1,5 @@
 exports.AuthRequiredCommand = require('streamhub-ui/auth-required-command');
-exports.BaseMenu = require('streamhub-ui/menu/base');
-exports.ShareMenu = require('streamhub-ui/menu/share');
+exports.BaseMenu = require('streamhub-ui/menu');
 exports.Button = require('streamhub-ui/button');
 exports.Command = require('streamhub-ui/command');
 exports.Container = require('streamhub-ui/container');
